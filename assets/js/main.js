354213099b0d3e27497e84fd4a9b45b536f35dbb
profilklader <116880397+profilklader@users.noreply.github.com>
2022-10-28 11:57:33 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+var $ = jQuery.noConflict();
+
+$(document).ready(function($) {
+    "use strict";
+
+    $('.animated').appear(function(){
+        var el = $(this);
+        var anim = el.data('animation');
+        var animDelay = el.data('delay');
+        if (animDelay) {
+
+            setTimeout(function(){
+                el.addClass( anim + " in" );
+                el.removeClass('out');
+            }, animDelay);
+
+        }
+
+        else {
+            el.addClass( anim + " in" );
+            el.removeClass('out');
+        }    
+    },{accY: -150});  
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            $('.back-to-top').fadeIn();
+        } else {
+            $('.back-to-top').fadeOut();
+        }
+    });
+
+    $('.back-to-top').click(function () {
+        $('html, body').animate({
+            scrollTop: 0,
+            easing: 'swing'
+        }, 750);
+        return false;
+    });
+
+
+
+});
+
+$(window).load(function(){
+    "use strict";
+
+    $("#loading-animation").fadeOut();
+
+    $("#preloader").delay(600).fadeOut("slow");
+
+});
